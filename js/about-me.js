5367c4538ef52d5aca2e2d28a1c9ca6ed90580ea
Nathan Carpenter <nathan.carpenter03@gmail.com>
2017-02-13 15:37:23 -0500

    added madlib, added adventure/epic mode for about-me

--- js/about-me.js
+++ js/about-me.js
@@ -1,3 +1,5 @@
+var moment = require('moment');
+
 var firstName = 'Nathan';
 var lastName= 'Carpenter';
 var fullName = firstName + ' ' + lastName;
@@ -5,18 +7,24 @@ var email = 'nathan.carpenter03@gmail.com';
 var grewUpInCity = true;
 var numYearsInCollege = 4;
 var numPets = 1;
-var numHoursSleepAvg= 7.5;
+var numHoursSleepAvg = 7.5;
 var favMusicGenre= 'trance';
 var favShowsAndMovies = ['Breaking Bad', 'Game of Thrones', 'The Wire', 'Pulp Fiction'];
 var jobTitleIn5Years;
 var lifeWithoutJS = null;
 
-var GraduationDate = {
-    year: 2005,
-    month: 'June'
-};
+// Adventure Mode
 
-console.log (firstName, lastName, fullName, email, grewUpInCity, numYearsInCollege, numPets, numHoursSleepAvg, favMusicGenre, favShowsAndMovies, jobTitleIn5Years, lifeWithoutJS, GraduationDate.year);
+// var graduationDate = new Date(graduationYear);
+
+// var graduationDate = new Date();
+// graduationDate.setFullYear(2005);
+// graduationDate = graduationDate.getFullYear();
 
 // Epic Mode
+
+var graduationDate = moment().format('2005');
+
+
+console.log (firstName, lastName, fullName, email, grewUpInCity, numYearsInCollege, numPets, numHoursSleepAvg, favMusicGenre, favShowsAndMovies, jobTitleIn5Years, lifeWithoutJS, graduationDate);
 
