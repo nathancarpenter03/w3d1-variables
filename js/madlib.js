374314dfffb117f2cbab7307ea1864ea47094544
Nathan Carpenter <nathan.carpenter03@gmail.com>
2017-02-13 16:29:18 -0500

    added aventure mode for madlib

--- js/madlib.js
+++ js/madlib.js
@@ -1,9 +1,17 @@
+var male = true;
+
 var noun1 = 'box';
 var noun2 = 'cheeto';
 var noun3 = 'circus';
 var noun4 = 'talk show';
 var noun5 = 'arrows';
-var name1= 'Steven';
+
+if (male = true) {
+    var name1 = 'Steven';
+} else {
+    var name1 = 'Marie';
+};
+
 var name2 = 'Andrea';
 var verb1 = 'jump rope';
 var verb2 = 'bake';
@@ -18,10 +26,10 @@ var adjective4 = 'drippy';
 var adjective5 = 'velcrowey';
 var adjective6 = 'interesting';
 
-
 var story = ` ${name1} is a ${adjective1} and ${adjective2} ${noun1} who likes to ${verb1} all day long. But one day they met a new friend named ${name2} and everything changed. ${name2} is a ${adjective3} and ${adjective4} ${noun2} who hated to ${verb3}, but loves to ${verb4}. They became best friends and now enjoy ${verb5} together. On Mondays they play with their ${adjective5} ${noun3} and go to the ${adjective6} ${noun4}. Once they collect all their ${noun5}, they will ${verb6} happily ever after. ` ;
 
 console.log(story);
+
 
 
 
