e3182522cb74f40fbf20481040271deb2aded331
Nathan Carpenter <nathan.carpenter03@gmail.com>
2017-02-13 14:36:53 -0500

    fixed object

--- js/about-me.js
+++ js/about-me.js
@@ -5,22 +5,18 @@ var email = 'nathan.carpenter03@gmail.com';
 var grewUpInCity = true;
 var numYearsInCollege = 4;
 var numPets = 1;
-var numHoursSleepAvg= 7;
+var numHoursSleepAvg= 7.5;
 var favMusicGenre= 'trance';
 var favShowsAndMovies = ['Breaking Bad', 'Game of Thrones', 'The Wire', 'Pulp Fiction'];
 var jobTitleIn5Years;
 var lifeWithoutJS = null;
 
-console.log (firstName, lastName, fullName, email, grewUpInCity, numYearsInCollege, numPets, numHoursSleepAvg, favMusicGenre, favShowsAndMovies, jobTitleIn5Years, lifeWithoutJS);
-
-
-// Adventurer Mode 
-
-var graduationYear = 2009;
-
-var Date = {
-    GraduationYear: 2009
+var GraduationDate = {
+    year: 2005,
+    month: 'June'
 };
+
+console.log (firstName, lastName, fullName, email, grewUpInCity, numYearsInCollege, numPets, numHoursSleepAvg, favMusicGenre, favShowsAndMovies, jobTitleIn5Years, lifeWithoutJS, GraduationDate.year);
 
 // Epic Mode
 
---
+++ js/madlib.js
@@ -0,0 +1,28 @@
+var noun1 = 'box';
+var noun2 = 'cheeto';
+var noun3 = 'circus';
+var noun4 = 'talk show';
+var noun5 = 'arrows';
+var name1= 'Steven';
+var name2 = 'Andrea';
+var verb1 = 'jump rope';
+var verb2 = 'bake';
+var verb3 = 'throw';
+var verb4 = 'laugh';
+var verb5 = 'burying';
+var verb6 = 'give';
+var adjective1 = 'moldy';
+var adjective2 = 'salmon';
+var adjective3 = 'bright';
+var adjective4 = 'drippy';
+var adjective5 = 'velcrowey';
+var adjective6 = 'interesting';
+
+
+var story = ` ${name1} is a ${adjective1} and ${adjective2} ${noun1} who likes to ${verb1} all day long. But one day they met a new friend named ${name2} and everything changed. ${name2} is a ${adjective3} and ${adjective4} ${noun2} who hated to ${verb3}, but loves to ${verb4}. They became best friends and now enjoy ${verb5} together. On Mondays they play with their ${adjective5} ${noun3} and go to the ${adjective6} ${noun4}. Once they collect all their ${noun5}, they will ${verb6} happily ever after. ` ;
+
+console.log(story);
+
+
+
+
