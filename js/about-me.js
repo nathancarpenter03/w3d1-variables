80dfded626f929b735db5e2c6f52bfa29a25f872
Nathan Carpenter <nathan.carpenter03@gmail.com>
2017-02-13 12:35:25 -0500

    added about-me.js file

---
+++ js/about-me.js
@@ -0,0 +1,26 @@
+var firstName = 'Nathan';
+var lastName= 'Carpenter';
+var fullName = firstName + ' ' + lastName;
+var email = 'nathan.carpenter03@gmail.com';
+var grewUpInCity = true;
+var numYearsInCollege = 4;
+var numPets = 1;
+var numHoursSleepAvg= 7;
+var favMusicGenre= 'trance';
+var favShowsAndMovies = ['Breaking Bad', 'Game of Thrones', 'The Wire', 'Pulp Fiction'];
+var jobTitleIn5Years;
+var lifeWithoutJS = null;
+
+console.log (firstName, lastName, fullName, email, grewUpinCity, numYearsInCollege, numPets, numHoursSleepAvg, favMusicGenre, favShowsAndMovies, jobTitleIn5Years, lifeWithoutJs);
+
+
+// Adventurer Mode 
+
+var graduationYear = 2009;
+
+var Date = {
+    GraduationYear: 2009;
+};
+
+// Epic Mode
+
